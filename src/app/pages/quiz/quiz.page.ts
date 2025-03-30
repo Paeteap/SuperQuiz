cc1ab738e6d9269eb3ae337c7b70fae83c9d691f
Patrick Olah <mt221027@fhstp.ac.at>
2025-03-30 18:44:37 +0200

    code optimierungen

--- src/app/pages/quiz/quiz.page.ts
+++ src/app/pages/quiz/quiz.page.ts
@@ -1,21 +1,22 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonButtons, IonBackButton, IonItem } from '@ionic/angular/standalone';
 import { DataService } from 'src/app/services/data.service';
-import { ToastController } from '@ionic/angular';
-import { AlertController } from '@ionic/angular';
+import { ToastController, AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
-
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonButtons, IonBackButton, IonItem, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.page.html',
   styleUrls: ['./quiz.page.scss'],
   standalone: true,
-  imports: [IonBackButton, IonButtons, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonLabel, IonItem, IonBackButton, IonButtons,
+    IonButton, IonContent, IonHeader, IonTitle,
+    IonToolbar, CommonModule, FormsModule
+  ]
 })
-
 export class QuizPage implements OnInit {
   public data = inject(DataService);
   private toastController = inject(ToastController);
@@ -23,47 +24,24 @@ export class QuizPage implements OnInit {
   private router = inject(Router);
 
   public shuffledQuestions: any[] = [];
-  public pointer: number = 0;
+  public pointer = 0;
   public currentElement: any;
-  public score: number = 0;
-
-  constructor() {}
+  public score = 0;
 
   ngOnInit() {
     const originalQuestions = [...this.data.currentQuiz.questions];
     this.shuffledQuestions = this.shuffle(originalQuestions);
-
     this.pointer = 0;
     this.currentElement = this.shuffledQuestions[this.pointer];
   }
 
   private shuffle(array: any[]) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
+    return array.sort(() => Math.random() - 0.5);
   }
 
-  async nextQuestion() {
-    this.pointer++;
-    if (this.pointer < this.shuffledQuestions.length) {
-      this.currentElement = this.shuffledQuestions[this.pointer];
-    } else {
-      const alert = await this.alertController.create({
-        header: 'Quiz Finished!',
-        message: `You scored ${this.score} out of ${this.shuffledQuestions.length}.`,
-        buttons: [{
-          text: 'OK',
-          handler: () => {
-            this.router.navigateByUrl('/home');
-          }
-        }]
-      });
-  
-      await alert.present();
-    }
-  }  
+  private getCorrectKey(): string {
+    return 'a' + this.currentElement.correct;
+  }
 
   private async showToast(message: string, color: string) {
     const toast = await this.toastController.create({
@@ -71,21 +49,36 @@ export class QuizPage implements OnInit {
       duration: 1000,
       color,
     });
-  
     await toast.present();
   }
 
-  checkAnswer(selectedKey: string) {
-    const selectedAnswer = this.currentElement[selectedKey];
-    const correctKey = 'a' + this.currentElement.correct;
-    const isCorrect = selectedKey === correctKey;
-  
-    if (isCorrect) {
-      this.score++;
-    }
-  
-    this.showToast(isCorrect ? 'Correct! 🎉' : 'Wrong! ❌', isCorrect ? 'success' : 'danger');
+  async checkAnswer(selectedKey: string) {
+    const isCorrect = selectedKey === this.getCorrectKey();
+
+    if (isCorrect) this.score++;
+
+    await this.showToast(isCorrect ? 'Correct!' : 'Wrong!', isCorrect ? 'success' : 'danger');
+
     setTimeout(() => this.nextQuestion(), 1000);
   }
-}
 
+  async nextQuestion() {
+    this.pointer++;
+
+    if (this.pointer < this.shuffledQuestions.length) {
+      this.currentElement = this.shuffledQuestions[this.pointer];
+      return;
+    }
+
+    const alert = await this.alertController.create({
+      header: 'Quiz Finished!',
+      message: `You scored ${this.score} out of ${this.shuffledQuestions.length}.`,
+      buttons: [{
+        text: 'OK',
+        handler: () => this.router.navigateByUrl('/home')
+      }]
+    });
+
+    await alert.present();
+  }
+}
